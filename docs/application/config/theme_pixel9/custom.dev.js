6ab558bc020937e30a0c9a277eeb150cf3bdd160
shahroq <shahroq@yahoo.com>
2025-05-29 14:32:18 -0700

    V9.3.1

---
+++ docs/application/config/theme_pixel9/custom.dev.js
@@ -0,0 +1,12 @@
+/*-----------------------------------------------------------------------------------
+
+	custom.dev.js
+		1. Copy this file to /application/config/theme_pixel9/ folder. The 'theme_pixel9' folder does not exist as default and it should be created manually.)
+		2. Rename the copied file 'custom.dev.js' (The '.tmp' extension should be removed).
+		3. Add your custom js.
+
+-----------------------------------------------------------------------------------*/
+
+$( document ).ready(function() {
+    console.log('Pixel 9 custom js is loaded ...');
+});
